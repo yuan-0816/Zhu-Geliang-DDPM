a402dd979ebc970cd9ea961ed1bc7c56bc75c883
Yuan-0816 <wj582693@gmail.com>
2024-05-12 23:55:48 +0800

    try to use threads to speed up

--- Find_Zhu_Geliang.py
+++ Find_Zhu_Geliang.py
@@ -13,20 +13,47 @@ Zhu Geliang is alive!
                                                                                            #####
 """
 
-# TODO: 用其他更精準的人臉辨識模型 ex:DeepFace https://github.com/serengil/deepface
 # TODO: Thread 加快速度
+# TODO: 加入進度條
 
 import cv2
 import numpy as np
 import os
-from deepface import DeepFace
-import pandas as pd
-from PIL import Image
+import argparse
+from tqdm import tqdm
+import threading
+
+from InfoZhuGeliangDDPM import PrintEverything
+
+def split_list(input_list, num_groups):
+    avg = len(input_list) // num_groups
+    remainder = len(input_list) % num_groups
+    result = []
+    start = 0
+    for i in range(num_groups):
+        size = avg + 1 if i < remainder else avg
+        result.append(input_list[start:start+size])
+        start += size
+    return result
+
+
+def check_file_type(file_path: str) -> str:
+    img_formats = ['bmp', 'jpg', 'jpeg', 'png', 'tif', 'tiff', 'dng', 'webp']
+    vid_formats = ['mov', 'avi', 'mp4', 'mpg', 'mpeg', 'm4v', 'wmv', 'mkv']
+    
+    file_extension = file_path.split('.')[-1].lower()
+    
+    if file_extension in img_formats:
+        return 'image'
+    elif file_extension in vid_formats:
+        return 'video'
+    else:
+        return 'unknown'
 
 
 def Train_Zhu_Geliang_Finder(image_folder_path: str, model_save_path: str) -> None:
     detector = cv2.CascadeClassifier(
-        "Zhu_Geliang_datasets/Find_Zhu_Geliang_model/haarcascade_frontalface_default.xml"
+        "Find_Zhu_Geliang_model/haarcascade_frontalface_default.xml"
     )
     recog = cv2.face.LBPHFaceRecognizer_create()  # 啟用訓練人臉模型方法
     faces = []  # 儲存人臉位置大小的串列
@@ -50,9 +77,7 @@ def Train_Zhu_Geliang_Finder(image_folder_path: str, model_save_path: str) -> No
                 img_np = np.array(gray, "uint8")  # 轉換成指定編碼的 numpy 陣列
                 face = detector.detectMultiScale(gray)  # 擷取人臉區域
                 for x, y, w, h in face:
-                    faces.append(
-                        img_np[y : y + h, x : x + w]
-                    )  # 記錄蔡英文人臉的位置和大小內像素的數值
+                    faces.append(img_np[y : y + h, x : x + w])
                     ids.append(1)
             else:
                 print(f"無法讀取檔案: {file_path}")
@@ -62,105 +87,117 @@ def Train_Zhu_Geliang_Finder(image_folder_path: str, model_save_path: str) -> No
     print("ok!")
 
 
-def Zhu_Geliang_Finder(image_folder_path: str, OutPut_path: str) -> None:
-    detector = cv2.CascadeClassifier(
-        "Zhu_Geliang_datasets/Find_Zhu_Geliang_model/haarcascade_frontalface_default.xml"
-    )
-    recog = cv2.face.LBPHFaceRecognizer_create()  # 啟用訓練人臉模型方法
-    recog.read(
-        "Zhu_Geliang_datasets/Find_Zhu_Geliang_model/Zhu_Geliang_face.yml"
-    )  # 讀取訓練好的模型
+def Zhu_Geliang_Finder(folder_path: str, file_list: list ,OutPut_path: str, Show_img: bool, detector, recog) -> None:
+    if file_list:
+        for file_name in tqdm(file_list, desc="Processing images"):
+            file_path = os.path.join(folder_path, file_name)
 
-    file_list = os.listdir(image_folder_path)
+            if check_file_type(file_path) == 'image':
+                img = cv2.imread(file_path)
+                if img is not None:
+                    show_img = img.copy()
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 轉換成黑白
+                    faces = detector.detectMultiScale(
+                        gray
+                    )  # 追蹤人臉 ( 目的在於標記出外框 )
 
-    for file_name in file_list:
-        # 檢查檔案是否為 JPG 格式
-        if file_name.endswith(".jpg") or file_name.endswith(".JPG"):
-            # 組合完整的檔案路徑
-            file_path = os.path.join(image_folder_path, file_name)
+                    face_index = 0
+                    for x, y, w, h in faces:
+                        cv2.rectangle(show_img, (x, y), (x+w, y+h), (0, 255, 0), 2)            # 標記人臉外框
+                        idnum, confidence = recog.predict(
+                            gray[y : y + h, x : x + w]
+                        )  # 取出 id 號碼以及信心指數 confidence
+                        if confidence < 80:
+                            text = "Zhu Geliang"
+                            crop_save_path = (
+                                OutPut_path + "/" + f"{file_name[:-4]}_{face_index}.jpg"
+                            )
+                            cv2.imwrite(str(crop_save_path), img)
+                            face_index += 1
+                        else:
+                            text = "???"
+                        # 在人臉外框旁加上名字
+                        cv2.putText(show_img, text, (x,y-5),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+                    if Show_img:
+                        cv2.imshow('oxxostudio', show_img)
+                        if cv2.waitKey(0) == ord('q'):
+                            break
+            else:
+                continue
 
-            # 使用 OpenCV 讀取圖片
-            img = cv2.imread(file_path)
+def process_files_threaded(file_lists, folder_path, output_path, show_img, detector, recog):
+    threads = []
+    for file_list in file_lists:
+        print(file_list)
+    #     thread = threading.Thread(target=Zhu_Geliang_Finder, args=(file_list, folder_path, output_path, show_img, detector, recog))
+    #     thread.start()
+    #     threads.append(thread)
 
-            # 檢查圖片是否成功讀取
-            if img is not None:
-                show_img = img.copy()
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 轉換成黑白
-                faces = detector.detectMultiScale(
-                    gray
-                )  # 追蹤人臉 ( 目的在於標記出外框 )
-
-                face_index = 0
-                for x, y, w, h in faces:
-                    # cv2.rectangle(show_img, (x,y), (x+w,y+h), (0,255,0), 2)            # 標記人臉外框
-                    idnum, confidence = recog.predict(
-                        gray[y : y + h, x : x + w]
-                    )  # 取出 id 號碼以及信心指數 confidence
-                    if confidence < 60:
-                        text = "Zhu Geliang"
-                        crop_save_path = (
-                            OutPut_path + "/" + f"{file_name[:-4]}_{face_index}.jpg"
-                        )  # 如果信心指數小於 60，取得對應的名字
-                        cv2.imwrite(str(crop_save_path), img)
-                        face_index += 1
-                    else:
-                        text = "???"
-                    # 在人臉外框旁加上名字
-                    # cv2.putText(show_img, text, (x,y-5),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
-
-                # cv2.imshow('oxxostudio', show_img)
-                # if cv2.waitKey(0) == ord('q'):
-                #     break
-
-
-
-
-def UseDeepFace():
-    img_path = "Zhu_Geliang_datasets/Face_image/test"
-    db_path = "Zhu_Geliang_datasets/Zhu_Geliang_face/test"
-    new_file_path = "Zhu_Geliang_datasets/Zhu_Geliang_face/test_deepface"
-
-
-    # try:
-    #     dfs = DeepFace.find(img_path = img_path, db_path = db_path)
-    #     print(dfs)
-    # except:
-    #     pass
-
-    # 取得資料夾中的所有檔案
-    file_list = os.listdir(img_path)
-
-    for file_name in file_list:
-        # 檢查檔案是否為 JPG 格式
-        if file_name.endswith(".jpg") or file_name.endswith(".JPG"):
-            
-            file_path = os.path.join(image_folder_path, file_name)
-
-            dfs = DeepFace.find(img_path = file_path, db_path = db_path, detector_backend = "yolov8", enforce_detection=False)
-            # print(dfs) # 印出所有內容
-            # print(dfs.index) # 印出索引
-            try:
-                if dfs[0]["threshold"].iloc[0] > 0.5:
-                    img = Image.open(file_path)
-                    # 新的檔案路徑
-                    new_file_path = "/" + file_name  # 在原始檔名前加上"new_"
-                    # 儲存圖片
-                    img.save(new_file_path)
-            except:
-                pass
-
-
+    # for thread in threads:
+    #     thread.join()
 
 
 if __name__ == "__main__":
-    model_save_path = "Zhu_Geliang_datasets/Find_Zhu_Geliang_model"
-    image_folder_path = "Zhu_Geliang_datasets/Face_image/exp"
-    OutPut_path = "Zhu_Geliang_datasets/Zhu_Geliang_face/test"
-    
-    # Train_Zhu_Geliang_Finder(image_folder_path, model_save_path)
-    # Zhu_Geliang_Finder(image_folder_path, OutPut_path)
+    PrintEverything()
+    parser = argparse.ArgumentParser(prog="Find_Zhu_Geliang.py")
+    parser.add_argument("--is-train", type=bool, default=False, help="是否訓練模型")
+    parser.add_argument("--view-img", type=bool, default=False, help="是否顯示圖片")
+    parser.add_argument(
+        "--source-folder",
+        type=str,
+        default="Zhu_Geliang_datasets/Face_image/exp",
+        # default="Zhu_Geliang_datasets/Zhu_Geliang_face/test2",
+        help="目標資料夾路徑",
+    )
+    parser.add_argument(
+        "--output-path",
+        type=str,
+        default="Zhu_Geliang_datasets/Zhu_Geliang_face/test2",
+        help="輸出資料夾路徑",
+    )
+    parser.add_argument(
+        "--model-save-path",
+        type=str,
+        default="Find_Zhu_Geliang_model",
+        help="模型儲存路徑",
+    )
+    parser.add_argument(
+    "--train-image-path",
+    type=str,
+    default="Zhu_Geliang_datasets/Zhu_Geliang_face/test",
+    help="訓練模型資料夾路徑",
+    )
+    parser.add_argument(
+    "--num-threads",
+    type=int,
+    default=30,
+    help="處理的執行續數量",
+    )
+    args = parser.parse_args()
 
-    # TODO: 改用 DeepFace 來辨識
-    # UseDeepFace()
-    from PrintZhuGeliangName import printZhuGeliang
-    printZhuGeliang()
+
+    if args.is_train:
+        Train_Zhu_Geliang_Finder(args.train_image_path, args.model_save_path)
+    else:
+        my_list = os.listdir(args.source_folder)
+        num_groups = args.num_threads
+        result = split_list(my_list, num_groups)
+        # print(result)
+        detector = cv2.CascadeClassifier(
+            "Find_Zhu_Geliang_model/haarcascade_frontalface_default.xml"
+        )
+        recog = cv2.face.LBPHFaceRecognizer_create()
+        recog.read("Find_Zhu_Geliang_model/Zhu_Geliang_face.yml")
+
+
+
+        process_files_threaded(result, args.source_folder, args.output_path, args.view_img, detector, recog)
+
+        # Zhu_Geliang_Finder(args.source_folder, result[6], args.output_path, args.view_img, detector, recog)
+        
+
+
+
+
+        
+
---
+++ InfoZhuGeliangDDPM.py
@@ -0,0 +1,33 @@
+"""
+Yuan @ 2024.05.12
+This is a script to find Zhu Geliang.
+Zhu Geliang is alive!
+"""
+
+def PrintInfo():
+    print('------------------------------------------------------------------------')
+    print('This program is made by Yuan')
+    print('It is a generative model implemented using DDIM to gernerate Zhu Geliang')
+    print('Zhu Geliang is a living being!')
+    print('------------------------------------------------------------------------')
+
+def PrintZhuGeliang():
+    artwork = """
+     #######  ###                          ####             ###       ##                                           ##
+     #   ##    ##                         ##  ##             ##                                                   ####
+        ##     ##      ##  ##            ##        ####      ##      ###      ####    #####     ### ##            ####
+       ##      #####   ##  ##            ##       ##  ##     ##       ##         ##   ##  ##   ##  ##              ##
+      ##       ##  ##  ##  ##            ##  ###  ######     ##       ##      #####   ##  ##   ##  ##              ##
+     ##    #   ##  ##  ##  ##             ##  ##  ##         ##       ##     ##  ##   ##  ##    #####
+     #######  ###  ##   ######             #####   #####    ####     ####     #####   ##  ##       ##              ##
+                                                                                               #####
+    """
+    print(artwork)
+
+def PrintEverything():
+    PrintZhuGeliang()
+    PrintInfo()
+
+
+if __name__ == '__main__':
+    PrintEverything()
